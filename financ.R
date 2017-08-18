2a521d36a98d3a8d8d80519964d8e99c9d576b72
mgaldino <mcz.fea@gmail.com>
2017-08-18 18:22:35 -0300

    usei clean_names versão dev (1.6), e removi acentos no script (referências a nomes de colunas), reorganizei importação de dados (no começo do script) e apaguei validações que não são mais necessárias.

--- financ.R
+++ financ.R
@@ -6,15 +6,19 @@ setwd("C:/Users/mgaldino/2017/Geral TB/Advocacy/reforma_eleitoral")
 
 ## carregando bases de dados
 # 12
-rec12 <- fread("2012/receitas_candidatos_2012_brasil.txt")
-rec12 <- rec12 %>%
-  clean_names()
-
 
 # 16
-receitas16 <- fread("2016/despesas_partidos_prestacao_contas_final_2016_brasil.txt")
+receitas16 <- fread("2016/receitas_candidatos_prestacao_contas_final_2016_brasil.txt")
 receitas16 <- receitas16 %>%
   clean_names()
+
+# 14
+rec14 <- fread("2014/receitas_candidatos_2014_brasil.txt")
+
+##renoeando colunas
+rec14 <- rec14 %>%
+  clean_names()
+
 
 # cnpj
 cnpj2015 <- read_fwf(
@@ -48,16 +52,7 @@ rec16_r <- rec16_r %>%
                                       bol_juridica == "PF" & tipo_receita == "Recursos próprios" ~ "recursos próprios",
                                       bol_juridica == "PF" & tipo_receita != "Recursos próprios" ~ "PF outros"))
 
-rec16_r %>%
-  group_by(tipo_receita, bol_partido, comite, comite2) %>%
-  summarise(contagem = n(),
-            gasto = sum(as.numeric(gsub(",", "\\.", valor_receita)))) %>%
-  ungroup() %>%
-  mutate(total_gasto = sum(gasto),
-         perc_gasto = round(gasto/total_gasto, 2))
 
-
-        
 resultado16 <- rec16_r%>%
   group_by(tipo_recurso_tb) %>%
   summarise(contagem = n(),
@@ -66,34 +61,23 @@ resultado16 <- rec16_r%>%
   mutate(total_gasto = sum(gasto),
          perc_gasto = round(gasto/total_gasto, 2))
 
-View(resultado16)
+resultado16
 
 ###############################
 ####2) Cálculo tipos de receita em 2012 (pronto)
 ###############################
 
-rec12 <- fread("receitas_candidatos_2012_brasil.txt")
+rec12 <- fread("2012/receitas_candidatos_2012_brasil.txt")
 
-##renoeando colunas
+##renomeando colunas
 rec12 <- rec12 %>%
   clean_names()
 
 ##composição doações
-teste1 <- rec12_r %>%
-  group_by(tipo_receita, bol_juridica, bol_partido, comite, comite2, tipo_recurso_tb) %>%
-  summarise(contagem = n(),
-            gasto = sum(as.numeric(gsub(",", "\\.", valor_receita)))) %>%
-  ungroup() %>%
-  mutate(total_gasto = sum(gasto),
-         perc_gasto = round(gasto/total_gasto, 2))
-
-View(teste1)
-
-##selecionando rec12 e combinando cnpj2015
 
 rec12_r <- rec12 %>%
   select(sequencial_candidato, numero_recibo_eleitoral, cpf_do_candidato, nome_do_doador,
-         valor_receita, tipo_receita, fonte_recurso, cpf_cnpj_do_doador, setor_econômico_do_doador,
+         valor_receita, tipo_receita, fonte_recurso, cpf_cnpj_do_doador, setor_economico_do_doador,
          nome_do_doador_receita_federal) %>%
   mutate(bol_juridica = ifelse(nchar(cpf_cnpj_do_doador) <= 11, "PF", "PJ")) %>%
   left_join(cnpj2015, 
@@ -111,6 +95,7 @@ rec12_r <- rec12 %>%
                                       tipo_receita != "Recursos de outros candidatos/comitês" & bol_juridica == "PJ" & bol_partido == 0 & comite2 == "outro" & comite == "outro" ~ 'empresas'))
 
 
+##selecionando rec12 e combinando cnpj2015
 resultado12 <- rec12_r%>%
   group_by(tipo_recurso_tb) %>%
   summarise(contagem = n(),
@@ -119,7 +104,7 @@ resultado12 <- rec12_r%>%
   mutate(total_gasto = sum(gasto),
          perc_gasto = round(gasto/total_gasto, 2))
 
-View(resultado12)
+resultado12
 
 
 ###não esquecer de projetar outros cenários deslocando os
@@ -130,11 +115,6 @@ View(resultado12)
 ####3) Cálculo tipos de receita em 2014 (pronto)
 ##############
 
-rec14 <- fread("receitas_candidatos_2014_brasil.txt")
-
-##renoeando colunas
-rec14 <- rec14 %>%
-  clean_names()
 
 ##composição doações
 rec14 %>%
@@ -147,30 +127,20 @@ rec14 %>%
 
 ##pegando CNPJ dos diretórios
 
-library(readr)
-
-cnpj2015 <- read_fwf(
-  file="CNPJ_diretorios_partidarios_2015.txt",  
-  skip=1,
-  fwf_widths(c(1, 2, 14, 150),
-             c("registro", "tipo", "cnpj", "nome_partido")))
-
-rec14_backup <- rec14
-
 rec14_r <- rec14 %>%
-  select(sequencial_candidato, numero_recibo_eleitoral, cpf_do_candidato, nome_do_doador_originário, nome_do_doador,
+  select(sequencial_candidato, numero_recibo_eleitoral, cpf_do_candidato, nome_do_doador_originario, nome_do_doador,
          valor_receita, tipo_receita, fonte_recurso, cpf_cnpj_do_doador,
-         cpf_cnpj_do_doador_originário) %>%
-  mutate(cnpj_final = ifelse(cpf_cnpj_do_doador_originário == "#NULO",
-                             cpf_cnpj_do_doador, cpf_cnpj_do_doador_originário)) %>%
+         cpf_cnpj_do_doador_originario) %>%
+  mutate(cnpj_final = ifelse(cpf_cnpj_do_doador_originario == "#NULO",
+                             cpf_cnpj_do_doador, cpf_cnpj_do_doador_originario)) %>%
   left_join(cnpj2015, 
              by=c('cnpj_final'='cnpj')) %>%
   mutate(bol_partido = as.numeric(!is.na(tipo)))
 
 rec14_r <- rec14_r %>%
   mutate(bol_juridica = ifelse(nchar(cnpj_final) <= 11, "PF", "PJ"),
-         comite = ifelse(grepl("comit", tolower(nome_do_doador_originário)), "comitê", "outro"),
-         comite2 = ifelse(grepl("comit", tolower(nome_do_doador)) & bol_juridica == "PJ" & cpf_cnpj_do_doador_originário == "#NULO", "comitê", "outro"),
+         comite = ifelse(grepl("comit", tolower(nome_do_doador_originario)), "comitê", "outro"),
+         comite2 = ifelse(grepl("comit", tolower(nome_do_doador)) & bol_juridica == "PJ" & cpf_cnpj_do_doador_originario == "#NULO", "comitê", "outro"),
          tipo_recurso_tb = case_when( bol_juridica == "PJ" & bol_partido == 0  & grepl("outro", comite) & grepl("outro", comite2) ~ "empresas", # & grepl("outro", comite) & grepl("outro", comite2) 
                                       tipo_receita != "Recursos próprios" & bol_juridica == "PF" & grepl("outro", comite) & grepl("outro", comite2) ~ "PF outros",
                                       tipo_receita == "Recursos próprios" & bol_juridica == "PF" ~ "Recursos Próprios",
@@ -194,4 +164,4 @@ resultado14 <- rec14_r%>%
   mutate(total_gasto = sum(gasto),
          perc_gasto = round(gasto/total_gasto, 2))
 
-View(resultado14)
+resultado14
